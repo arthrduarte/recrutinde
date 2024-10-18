9d308ed8c16a38781d05cf456c7d2c4d94be1782
Arthur Duarte <tuiduarte@gmail.com>
2024-10-18 15:20:23 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <object data="../ArthurDuarte_Resume.pdf" type="application/pdf" className='w-1/2'></object>
+      </div>
+    </>
+  )
+}
+
+export default App
